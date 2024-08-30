c80f9d7e08fea3f9f5a8f9022823e07e5844ecb9
Wesley Zhang <wesleyshsjc@gmail.com>
2024-08-30 12:24:58 -0700

    v1.2 dev, enabled switching from popup to sidepanel

---
+++ sidepanel.js
@@ -0,0 +1,12 @@
+import {consoleLog, LOG_LEVELS} from './scripts/utils';
+
+document.addEventListener ('DOMContentLoaded', () => {
+  const button = document.getElementById ('togglePopupButton');
+  button.addEventListener ('click', () => {
+    chrome.storage.local.set ({preferSidePanel: false}, () => {
+      chrome.action.setPopup ({popup: 'popup.html'}, () => {
+        window.close (); // Closes the side panel
+      });
+    });
+  });
+});
